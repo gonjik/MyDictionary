d70fff4782d2f0b03315df873bea02b9e8ee889b
gonjik <77414683+gonjik@users.noreply.github.com>
2021-01-23 20:03:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(110, "Gonca");
+            myDictionary.Add(120, "Etem");
+            myDictionary.Add(130, "Ateş");
+            myDictionary.Add(140, "Kaptan");
+            myDictionary.Say();
+           
+        }
+    }
+}
